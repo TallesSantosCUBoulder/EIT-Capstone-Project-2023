54ce8d5a794df07a9e2473a37b560c9fb6f2a21b
Diego Sena <dise4888@colorado.edu>
2023-12-07 15:51:47 -0700

    Python Connection

--- EIT_Python_Test/NI_Test.py
+++ EIT_Python_Test/NI_Test.py
@@ -1,2 +1,12 @@
-import nidaqmx as DAQ
+import nidaqmx
+import matplotlib.pyplot as plt
+import numpy as np
 
+with nidaqmx.Task() as task:
+    task.ai_channels.add_ai_voltage_chan("Dev2/ai0", terminal_config=nidaqmx.constants.TerminalConfiguration.RSE)
+    data = task.read(number_of_samples_per_channel=int(10e3))
+
+ax = plt.subplot()
+x = np.linspace(0, 1/10, int(10e3))
+ax.plot(x, data, linewidth = 2.0)
+plt.show()
