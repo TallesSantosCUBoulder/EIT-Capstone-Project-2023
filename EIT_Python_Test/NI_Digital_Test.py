19377937b14f5edcb3346499cd51a33dfbae2244
Diego Sena <dise4888@colorado.edu>
2024-01-09 13:07:33 -0700

    channel swtiching

--- EIT_Python_Test/NI_Digital_Test.py
+++ EIT_Python_Test/NI_Digital_Test.py
@@ -2,7 +2,12 @@ import nidaqmx
 import nidaqmx.constants as const
 import matplotlib.pyplot as plt
 import numpy as np
+import time
+while True:
+    with nidaqmx.Task() as task:
+        task.do_channels.add_do_chan("Dev1/port0/line0:4")
+        task.write(0b11)
+        time.sleep(1)
+        task.write(0b01)
+        time.sleep(1)
 
-with nidaqmx.Task() as task:
-    task.do_channels.add_do_chan("Dev1/port0/line0:4")
-    data = task.write(4)
