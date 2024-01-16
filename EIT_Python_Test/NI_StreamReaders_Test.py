a3ba9818baa6848ac0108e2a6b7550be3f8cef32
Diego Sena <dise4888@colorado.edu>
2024-01-16 14:25:33 -0700

    Working on continous read

--- EIT_Python_Test/NI_StreamReaders_Test.py
+++ EIT_Python_Test/NI_StreamReaders_Test.py
@@ -6,22 +6,22 @@ import numpy as np
 numSamples = 512
 data = np.zeros(numSamples)
 time = 1.0/125e3*numSamples
+ax = plt.subplot()
 
 with daq.Task(new_task_name='read') as read:
     read.ai_channels.add_ai_voltage_chan('Dev2/ai0', terminal_config=TerminalConfiguration.RSE)
-    read.timing.cfg_samp_clk_timing(125e3, sample_mode=AcquisitionType.CONTINUOUS, samps_per_chan=numSamples)
-    reader = streamRead.AnalogSingleChannelReader(read.in_stream)
-
-
+    read.timing.cfg_samp_clk_timing(125e3, sample_mode=AcquisitionType.FINITE, samps_per_chan=numSamples)
+    read.in_stream.input_buf_size = 512
+    
     def reading_task_callback(task_idx, event_type, num_samples, callback_data=None):
-        samps = reader.read_many_sample(data,number_of_samples_per_channel=numSamples)
-        ax = plt.subplot()
+        reader = streamRead.AnalogSingleChannelReader(read.in_stream)
+        samps = reader.read_many_sample(data, number_of_samples_per_channel=numSamples)
         x = np.linspace(0, time, samps)
         ax.plot(x, data, linewidth = 2.0)
-        plt.show()
 
         return 0
     
     read.register_every_n_samples_acquired_into_buffer_event(numSamples, reading_task_callback)
 
     read.start()
+    plt.show()
