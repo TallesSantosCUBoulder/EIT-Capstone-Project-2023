8a9b0d1d4d002e483a7eb66ee71773db723850fe
Diego Sena <diego.sena@colorado.edu>
2024-04-21 17:23:04 -0600

    Using ChatGPT to convert from MATLAB to Python

--- EIT_Python_Test/NI_Py_Task.py
+++ EIT_Python_Test/NI_Py_Task.py
@@ -5,29 +5,79 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
 from time import sleep
+from scipy.io import loadmat
+
+def set_mux(device, current_injection, skip_num, num_channels):
+    mux_set = [1] + list(reversed([int(x) for x in f'{current_injection % num_channels:04b}'])) + \
+              [1] + list(reversed([int(x) for x in f'{(current_injection + skip_num + 1) % num_channels:04b}']))
+    device.write(mux_set)
+
+def set_electrode(device, current_injection, skip_num, num_channels):
+    electrode_set = [int(x) for x in f'{current_injection:03b}']
+    electrode_set.extend([int(x) for x in f'{(current_injection + skip_num + 1) % num_channels:03b}'])
+    device.write(electrode_set)
+
+def compute_epiv(frq, N, sample_frq):
+    tk = np.arange(N) / sample_frq
+    w = 2 * np.pi * frq * tk[:, np.newaxis]
+    Etot = np.hstack((np.sin(w), np.cos(w), np.ones((N, len(frq)))))
+    return np.linalg.pinv(Etot)
+
+def multi_freq_demod(signal, Epiv, inject):
+    phi_tot = np.dot(Epiv, signal.T)
+    amp = np.sqrt(phi_tot[:, 0] ** 2 + phi_tot[:, 1] ** 2)
+    phase = np.arctan2(phi_tot[:, 1], phi_tot[:, 0])
+    phase -= phase[inject]
+    return np.real(amp * np.exp(1j * phase))
+
+def gather_frame(MuxDigiOut, SwitchSelect, dDAQ, num_channels, skip_num, N, Epiv):
+    volt_vec = np.zeros((num_channels ** 2,))
+    for i in range(num_channels):
+        set_mux(MuxDigiOut, i, skip_num, num_channels)
+        set_electrode(SwitchSelect, i, skip_num, num_channels)
+        time.sleep(300 / 110e3)
+        sig = dDAQ.read(N)
+        hold = multi_freq_demod(sig, Epiv, i)
+        volt_vec[i * num_channels: (i + 1) * num_channels] = hold[:num_channels]
+    return volt_vec
+
+## Initialize Variables
+numChannels = 8;                    # Number of electrodes in the system
+SkipN = 0;                          # Number of electrodes to skip
+adcRange = 1;                       # Sets the range of the adc
+currR = [10.03; 10.2];              # Resistance current is being measured over
+currG = [49.72; 53.05];             # Gain for current measurement
+N = 512;                            # Number of samples taken
+sampleRate = 110e3;                 # Sets the rate that the NI-Boards sample at
+outputRate = 1e6;                   # Sets the rate that the NI-Board output at
+bufferForPreLoad = 500e3;           # sets the length of buffer for preloading
+pkpV = 3;                           # pk-pk voltage per frequency
+frq = [20e3];                       # array of frequencies to make up multi-freq signal
+phase = [0 pi];                     # phase of signal 1 versus signal 2
+dFrq = -90.00548                    # Frequency Shift 
+
+# Load Reconstruction Matrix
+reconstruction = loadmat('reconstruction.mat')
+
+MatrixA_int = reconstruction['MatrixA_int']
+mask = reconstruction['mask']
+n_pixels = reconstruction['n_pixels']
+
+# Clear the loaded data (optional in Python)
+del reconstruction
 
 
-
-
-N = 512
-sampleRate = 110e3
 readTaskA = daq.Task('readTaskA') # Create analog read task
-#daq.system.System().connect_terms( # Connect the clocks together
-#    source_terminal='Dev1/ao/SampleClock',
-#    destination_terminal='Dev2/RTSI2')
-#daq.system.System().connect_terms( # Connect the triggers together
-#    source_terminal='Dev1/ao/StartTrigger',
-#    destination_terminal='Dev2/RTSI3')
 readTaskA.ai_channels.add_ai_voltage_chan('Dev1/ai0', terminal_config=TerminalConfiguration.RSE)
-#readTask.ai_channels.add_ai_voltage_chan('Dev2/ai0', terminal_config=TerminalConfiguration.RSE)
-#readTask.triggers.reference_trigger.dig_edge_src('Dev1/ai/SampleClock')
-#readTask.triggers.start_trigger.dig_edge_src('Dev1/ai/StartTrigger')
 readTaskA.timing.cfg_samp_clk_timing(sampleRate, sample_mode=AcquisitionType.CONTINUOUS)
+readerA.triggers.sync_type.MASTER = True
 readerA = readTaskA.in_stream
 
 readTaskB = daq.Task('readTaskB')
 readTaskB.ai_channels.add_ai_voltage_chan('Dev2/ai0', terminal_config=TerminalConfiguration.RSE)
-readTaskB.timing.cfg_samp_clk_timing(sampleRate, sample_mode=AcquisitionType.CONTINUOUS)
+readTaskB.timing.cfg_samp_clk_timing(source="Dev1/ai/SampleClock", sample_mode=AcquisitionType.CONTINUOUS)
+readTaskB.triggers.sync_type.SLAVE = True
+readTaskB.triggers.start_trigger.cfg_dig_edge_start_trig('Dev1/ai/StartTrigger')
 readerB = readTaskB.in_stream
 
 readTaskA.start()
@@ -52,12 +102,7 @@ def collectData(dataOut, readerA, readerB, numSamples):
 #     ax.set_ylabel('Voltage (V)')
 
 
-data = np.zeros([2,N])
-dT = 1.0/sampleRate*N
-x = np.linspace(0, dT, N)
-fig, ax = plt.subplots()
-data = collectData(data, readerA, readerB, N)
-data = np.rot90(data,1)
+
 #ani = animation.FuncAnimation(fig, animate, fargs=(data, reader, N, dT), interval=150, cache_frame_data=False)
 ax.plot(x, data, linewidth=2.0)
 plt.show()
@@ -66,4 +111,5 @@ readTaskA.stop()
 readTaskB.stop()
 readTaskA.close()
 readTaskB.close()
+
 
---
+++ Imaging_Control/Image_Generation.py
@@ -0,0 +1,125 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import nidaqmx
+import time
+
+def set_mux(device, current_injection, skip_num, num_channels):
+    mux_set = [1] + list(reversed([int(x) for x in f'{current_injection % num_channels:04b}'])) + \
+              [1] + list(reversed([int(x) for x in f'{(current_injection + skip_num + 1) % num_channels:04b}']))
+    device.write(mux_set)
+
+def set_electrode(device, current_injection, skip_num, num_channels):
+    electrode_set = [int(x) for x in f'{current_injection:03b}']
+    electrode_set.extend([int(x) for x in f'{(current_injection + skip_num + 1) % num_channels:03b}'])
+    device.write(electrode_set)
+
+def compute_epiv(frq, N, sample_frq):
+    tk = np.arange(N) / sample_frq
+    w = 2 * np.pi * frq * tk[:, np.newaxis]
+    Etot = np.hstack((np.sin(w), np.cos(w), np.ones((N, len(frq)))))
+    return np.linalg.pinv(Etot)
+
+def multi_freq_demod(signal, Epiv, inject):
+    phi_tot = np.dot(Epiv, signal.T)
+    amp = np.sqrt(phi_tot[:, 0] ** 2 + phi_tot[:, 1] ** 2)
+    phase = np.arctan2(phi_tot[:, 1], phi_tot[:, 0])
+    phase -= phase[inject]
+    return np.real(amp * np.exp(1j * phase))
+
+def gather_frame(MuxDigiOut, SwitchSelect, dDAQ, num_channels, skip_num, N, Epiv):
+    volt_vec = np.zeros((num_channels ** 2,))
+    for i in range(num_channels):
+        set_mux(MuxDigiOut, i, skip_num, num_channels)
+        set_electrode(SwitchSelect, i, skip_num, num_channels)
+        time.sleep(300 / 110e3)
+        sig = dDAQ.read(N)
+        hold = multi_freq_demod(sig, Epiv, i)
+        volt_vec[i * num_channels: (i + 1) * num_channels] = hold[:num_channels]
+    return volt_vec
+
+# Initialize Variables
+num_channels = 8
+skip_num = 0
+adc_range = 1
+curr_r = np.array([10.03, 10.2])
+curr_g = np.array([49.72, 53.05])
+N = 512
+sample_rate = 110e3
+output_rate = 1e6
+buffer_for_preload = 500e3
+pkpV = 3
+frq = np.array([20e3])
+phase = np.array([0, np.pi])
+dFrq = -90.00548
+
+# Load Reconstruction Matrix
+reconstruction = np.load('reconstruction.npy', allow_pickle=True).item()
+
+MatrixA_int = reconstruction['MatrixA_int']
+mask = reconstruction['mask']
+n_pixels = reconstruction['n_pixels']
+
+# Add Digital Mux Select
+MuxDigiOut = nidaqmx.Task()
+MuxDigiOut.do_channels.add_do_chan("Dev1/port1/line0:4")
+MuxDigiOut.do_channels.add_do_chan("Dev2/port1/line0:4")
+MuxDigiOut.stop()
+
+# Add Digital Switch Select
+SwitchSelect = nidaqmx.Task()
+SwitchSelect.do_channels.add_do_chan("Dev1/port2/line0:7")
+SwitchSelect.stop()
+
+# Add Analog Output
+dAOut = nidaqmx.Task()
+AnalogCH1 = dAOut.ao_channels.add_ao_voltage_chan("Dev1/ao0", min_val=-10.0, max_val=10.0)
+AnalogCH2 = dAOut.ao_channels.add_ao_voltage_chan("Dev2/ao0", min_val=-10.0, max_val=10.0)
+dAOut.stop()
+
+dAOut.triggers.start_trigger.cfg_dig_edge_start_trig(trigger_source="Dev1/RTSI0")
+dAOut.timing.cfg_samp_clk_shared_src(src_terminal="Dev1/RTSI1")
+dAOut.timing.cfg_samp_clk_timing(rate=output_rate)
+
+output_signal = np.load("outputSignal.npy")
+
+preload(dAOut, output_signal)
+
+# Add ADC Inputs
+dDAQ = nidaqmx.Task()
+dDAQ.ai_channels.add_ai_voltage_chan("Dev1/ai0", min_val=-adc_range, max_val=adc_range)
+dDAQ.ai_channels.add_ai_voltage_chan("Dev1/ai1", min_val=-adc_range, max_val=adc_range)
+dDAQ.ai_channels.add_ai_voltage_chan("Dev1/ai2", min_val=-adc_range, max_val=adc_range)
+dDAQ.ai_channels.add_ai_voltage_chan("Dev1/ai3", min_val=-adc_range, max_val=adc_range)
+dDAQ.ai_channels.add_ai_voltage_chan("Dev1/ai4", min_val=-adc_range, max_val=adc_range)
+dDAQ.ai_channels.add_ai_voltage_chan("Dev1/ai5", min_val=-adc_range, max_val=adc_range)
+dDAQ.ai_channels.add_ai_voltage_chan("Dev1/ai6", min_val=-adc_range, max_val=adc_range)
+dDAQ.ai_channels.add_ai_voltage_chan("Dev1/ai7", min_val=-adc_range, max_val=adc_range)
+dDAQ.ai_channels.add_ai_voltage_chan("Dev2/ai7", min_val=-2, max_val=2)
+dDAQ.ai_channels.add_ai_voltage_chan("Dev2/ai8", min_val=-2, max_val=2)
+
+dDAQ.timing.cfg_samp_clk_shared_src(src_terminal="Dev1/RTSI3")
+dDAQ.timing.cfg_samp_clk_timing(rate=sample_rate)
+
+# Main Loop
+dAOut.start(task="repeatoutput")
+
+Epiv = compute_epiv(frq + dFrq, N, sample_rate)
+empty_tank = gather_frame(MuxDigiOut, SwitchSelect, dDAQ, num_channels, skip_num, N, Epiv)
+
+while True:
+    volt_vec = gather_frame(MuxDigiOut, SwitchSelect, dDAQ, num_channels, skip_num, N, Epiv)
+    voltage_vec_diff = (volt_vec - empty_tank)
+    
+    imagem = np.real(np.dot(MatrixA_int, voltage_vec_diff).reshape(n_pixels, n_pixels) * mask)
+    plt.imshow(imagem.T, cmap='jet', origin='lower', aspect='equal')
+    plt.colorbar()
+    plt.show()
+
+    if not plt.fignum_exists(2):
+        print('Loop stopped by user')
+        break
+
+# Stop Outputs
+dAOut.stop()
+MuxDigiOut.stop()
+SwitchSelect.stop()
